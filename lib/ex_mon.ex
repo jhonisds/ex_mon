b3639f275c325263e4392e76a61675db0827a49e
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-08-25 17:35:20 -0300

    call struct at main module.

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -2,6 +2,7 @@ defmodule ExMon do
   @moduledoc """
   Documentation for `ExMon`.
   """
+  alias ExMon.Player
 
   @doc """
   Hello world.
@@ -12,7 +13,7 @@ defmodule ExMon do
       :world
 
   """
-  def hello do
-    :world
+  def create_player(name, move_avg, move_rnd, move_heal) do
+    Player.build(name, move_avg, move_rnd, move_heal)
   end
 end
